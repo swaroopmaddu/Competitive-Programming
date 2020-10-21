8b94e459bdac3984778eb7d48ec4f4583b45e580
Swaroop Maddu <madduswaroop@gmail.com>
2020-10-21 07:18:56 +0530

    Create Problem50A.java

---
+++ CodeForces/Problem50A.java
@@ -0,0 +1,14 @@
+/*
+url : https://codeforces.com/problemset/problem/50/A
+*/
+
+import java.util.Scanner;
+public class Problem50A {
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int m = in.nextInt();
+	    int n = in.nextInt();
+	    System.out.println((m*n)/2);
+	    in.close();
+	}
+}
