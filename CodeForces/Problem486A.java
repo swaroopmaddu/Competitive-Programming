5e4f85040ab481e4d6bb58c84968fd79d10ed306
Swaroop Maddu <madduswaroop@gmail.com>
2020-10-23 16:33:19 +0530

    Update Problem486A.java

--- CodeForces/Problem486A.java
+++ CodeForces/Problem486A.java
@@ -2,18 +2,16 @@
     https://codeforces.com/problemset/problem/486/A
 */
 import java.util.Scanner;
-public class Problem486A {
+public class Main {
 	public static void main(String[] args) {
 	    Scanner in = new Scanner(System.in);
-	    int n = in.nextInt();
-	    int result=0;
-	    while(n!=0){
-	        if(n%2==0){
-    	        result+=n;
-	        }else {
-	            result-=n;
-	        }
-	        n--;
+	    long n = in.nextLong();
+	    long result=0;
+	    
+	    if(n%2==0){
+	        result = n/2;
+	    } else {
+	        result = ((n + 1) / 2) * (-1);
 	    }
 	    System.out.println(result);
 	    in.close();
