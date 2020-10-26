8022b3dab37a27c3707132f46b8e7a7149f304f8
Swaroop Maddu <madduswaroop@gmail.com>
2020-10-26 13:08:34 +0530

    Create Problem1328A.java

---
+++ CodeForces/Problem1328A.java
@@ -0,0 +1,23 @@
+/*
+    https://codeforces.com/problemset/problem/1328/A
+*/
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int num = in.nextInt();
+	    
+	    while(num--!=0){
+	        int a = in.nextInt();
+	        int b = in.nextInt();
+	        if(a%b==0){
+	            System.out.println(0);
+	        }else{
+	            int n = (a/b+1)*b;
+	            System.out.println(n-a);
+	        }
+	    }
+	    
+	    in.close();
+	}
+}
