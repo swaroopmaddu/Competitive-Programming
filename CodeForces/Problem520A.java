2206ac56fa4b97c19621ad0fcaaeb874be3df910
Swaroop Maddu <madduswaroop@gmail.com>
2020-10-29 17:25:10 +0530

    Create Problem520A.java

---
+++ CodeForces/Problem520A.java
@@ -0,0 +1,26 @@
+/*
+    url https://codeforces.com/problemset/problem/520/A
+*/
+
+import java.util.Scanner;
+import java.util.HashSet;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int size = in.nextInt();
+	    String str = in.next();
+	    str = str.toLowerCase();
+	    HashSet<Character> chars = new HashSet<Character>();
+	    
+	    for(int i=0;i<size;i++){
+	        chars.add(str.charAt(i));
+	    }
+	    
+	    if(chars.size()==26){
+	        System.out.println("YES");
+	    } else {
+	        System.out.println("NO");
+	    }
+	    in.close();
+	}
+}
